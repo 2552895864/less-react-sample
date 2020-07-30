614054423ecb4e962820dd1e57c8396b508ed584
肖子立 <xiaozlb@digitalchina.com>
2020-07-30 14:13:36 +0800

    change derectory structure

---
+++ src/pages/homepage/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import APP from "../../components/APP";
+
+class HomePage extends Component {
+  render() {
+    return <APP />;
+  }
+}
+
+export default HomePage;
